56e7a552d9f01fe6b808eaf3c1abc4a122f9869a
tzwang1 <truman.wang1234@gmail.com>
2021-03-17 21:56:34 -0400

    Add a new handler to test.

--- server.go
+++ server.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func exampleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to example server!")
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
 	http.HandleFunc("/", exampleHandler)
+	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
