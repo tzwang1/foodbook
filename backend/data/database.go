4496ba9d18f57c06bf15a312d1438872f9f46397
tzwang1 <truman.wang1234@gmail.com>
2021-04-22 00:01:27 -0400

    Add functions to insert and delete a user.

--- backend/data/database.go
+++ backend/data/database.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"example_app/backend/data/models"
 	"fmt"
 	"log"
 	"os"
@@ -16,13 +17,28 @@ var user string = os.Getenv("POSTGRES_USER")
 var password string = os.Getenv("POSTGRES_PASSWORD")
 var dbname string = os.Getenv("POSTGRES_DB")
 
-func Connect() {
+type Database struct {
+	db *sql.DB
+}
+
+var instance *Database
+
+func GetDatabaseSingleton() *Database {
+	if instance == nil {
+		instance = &Database{db: initializeDatabase()}
+	}
+	return instance
+}
+
+func initializeDatabase() *sql.DB {
 	connected := false
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+	var db *sql.DB
+	var err error
 	for i := 0; i < 10; i++ {
-		db, err := sql.Open("postgres", psqlInfo)
+		db, err = sql.Open("postgres", psqlInfo)
 		if err != nil {
 			log.Println("Unable to Open DB: " + err.Error() + " ... Retrying\n")
 			time.Sleep(time.Second * 2)
@@ -34,10 +50,19 @@ func Connect() {
 			continue
 		}
 		connected = true
-
+		break
 	}
 	if !connected {
 		log.Panic("Unable to connect to database.")
 	}
 	log.Println("Successfully connected!")
+	log.Println("Initializing Tables...")
+	initializeTables(db)
+	return db
+}
+
+func initializeTables(db *sql.DB) (err error) {
+
+	_, err = db.Exec(models.INITIALIZE_USER_TABLE_QUERY)
+	return
 }
--- backend/data/models/user.go
+++ backend/data/models/user.go
@@ -5,7 +5,35 @@ import (
 )
 
 type User struct {
-	id   string
-	name string
-	age  int
+	id    string
+	name  string
+	age   int
+	email string
+}
+
+const TABLE_NAME = "users"
+
+const INITIALIZE_USER_TABLE_QUERY = `
+	CREATE TABLE IF NOT EXISTS` + TABLE_NAME + ` (
+	id serial PRIMARY KEY,
+	name text NOT NULL,
+	age integer
+	email integer)`
+
+func InsertUser(db *sql.DB, user User) (err error) {
+	sqlStatement := `
+	INSERT INTO $1 (name, age, email)
+	VALUES ($2, $3, $4)`
+	_, err = db.Exec(sqlStatement, TABLE_NAME, user.name, user.age, user.email)
+	return err
+}
+
+func DeleteUser(db *sql.DB, user User) (err error) {
+	sqlStatement := `
+	DELETE FROM $1 WHERE
+	name = $1 AND
+	age = $1 AND
+	email = $3`
+	_, err = db.Exec(sqlStatement, TABLE_NAME, user.name, user.age, user.email)
+	return err
 }
