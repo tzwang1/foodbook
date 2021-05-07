042820d82bc84721f2a6ba78ffe202cf2a6d67c4
tzwang1 <truman.wang1234@gmail.com>
2021-05-06 23:05:36 -0400

    Add function to get multiple recipes that match a name.

--- backend/data/models/recipe.go
+++ backend/data/models/recipe.go
@@ -12,7 +12,7 @@ type Recipe struct {
 
 const RECIPE_TABLE_NAME = "recipes"
 
-const INITIALIZE_USER_TABLE_QUERY = `
+const INITIALIZE_RECIPE_TABLE_QUERY = `
 	CREATE TABLE IF NOT EXISTS ` + RECIPE_TABLE_NAME + ` (
 	id serial PRIMARY KEY,
 	name text NOT NULL,
@@ -53,4 +53,23 @@ func GetRecipe(db *sql.DB, id string) (Recipe, error) {
 	default:
 		return Recipe{}, err
 	}
+}
+
+func GetRecipes(db *sql.DB, name string) ([]Recipe, error) {
+	sqlStatement := `
+	SELECt * FROM recipes where name LIKE $1`
+	var recipes []Recipe
+	rows, err := db.Query(sqlStatement, name)
+	if err != nil {
+		return recipes, err
+	}
+
+	for rows.Next() {
+		var recipe Recipe
+		if err := rows.Scan(&recipe.Id, &recipe.Name, &recipe.Rating); err != nil {
+			return recipes, err
+		}
+		recipes = append(recipes, recipe)
+	}
+	return recipes, nil
 }
